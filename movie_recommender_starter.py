339c94ffb8255253183255c4b0d1a72fcba13cab
jel7426 <jel7426@g.rit.edu>
2025-06-11 23:06:01 -0400

    removed side comments

--- movie_recommender_starter.py
+++ movie_recommender_starter.py
@@ -2,56 +2,36 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-###### helper functions. Use them when needed #######
 def get_title_from_index(index):
 	return df[df.index == index]["title"].values[0]
 
 def get_index_from_title(title):
 	return df[df.title == title]["index"].values[0]
-##################################################
 
-##Step 1: Read CSV File
 df = pd.read_csv('movie_dataset.csv')
-#print(df.columns)
 
-##Step 2: Select Features
 features = ['keywords', 'cast', 'genres', 'director']
 
-##Step 3: Create a column in DF which combines all selected features
 for feature in features:
 	df[feature] = df[feature].fillna("")
 
 def combine_features(row):
-	#try: 
 	return row['keywords'] + " " + row['cast'] + " " + row['genres'] + " " + row['director']
-	#except: 
-		#print("Error: " , row)
+	
 
 df["combined_features"] = df.apply(combine_features, axis = 1)
-#print(df["combined_features"].head())
 
-
-##Step 4: Create count matrix from this new combined column
 cv = CountVectorizer()
 count_matrix = cv.fit_transform(df["combined_features"])
 similarity_scores = cosine_similarity(count_matrix)
 
-##Step 5: Compute the Cosine Similarity based on the count_matrix
 
 movie_user_likes = "Avatar"
 
-## Step 6: Get index of this movie from its title
 index = get_index_from_title(movie_user_likes)
 
-## Step 7: Get a list of similar movies in descending order of similarity score
 movie_row = list(enumerate(similarity_scores[index]))
 descending_similarity = sorted(movie_row, key = lambda x:x[1], reverse=True)
 descending_similarity = descending_similarity[1:]
 for i in range(50):
 	print(get_title_from_index(descending_similarity[i][0]))
-
-## Step 8: Print titles of first 50 movies
-
-
-C:\Users\admin\Downloads\movie_recommender_starter.py
-
